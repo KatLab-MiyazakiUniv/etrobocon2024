1de38a7b1ccf1ae4eef55a47886f124945e7980c
takahashi <tkhs.tmhr39@gmail.com>
2024-05-29 17:39:05 +0900

    add: モータを動かす Controller.h, Controller.cppの追加

---
+++ module/API/Controller.cpp
@@ -0,0 +1,55 @@
+/**
+ * @file Controller.cpp
+ * @brief モーター制御に用いる関数をまとめたラッパークラス
+ * @author takahashitom
+ */
+#include "Controller.h"
+
+Controller::Controller() : rightWheel(PORT_B), leftWheel(PORT_C), armMotor(PORT_A) {}
+
+int Controller::limitPwmValue(const int value)
+{
+  if(value > MOTOR_PWM_MAX) {
+    return MOTOR_PWM_MAX;
+  } else if(value < MOTOR_PWM_MIN) {
+    return MOTOR_PWM_MIN;
+  }
+  return value;
+}
+
+// 右モータにPWM値をセット
+void Controller::setRightMotorPwm(const int pwm)
+{
+  rightWheel.setPWM(limitPwmValue(pwm));
+}
+
+// 左モータにPWM値をセット
+void Controller::setLeftMotorPwm(const int pwm)
+{
+  leftWheel.setPWM(limitPwmValue(pwm));
+}
+
+// タイヤのモータを停止する
+void Controller::stopMotor()
+{
+  leftWheel.stop();
+  rightWheel.stop();
+}
+
+// タイヤのモータのPWM値をリセットする
+void Controller::resetMotorPwm()
+{
+  leftWheel.reset();
+}
+
+// アームのモータにPWM値をセット
+void Controller::setArmMotorPwm(const int pwm)
+{
+  armMotor.setPWM(limitPwmValue(pwm));
+}
+
+// アームのモータを停止する
+void Controller::stopArmMotor()
+{
+  armMotor.stop();
+}
---
+++ module/API/Controller.h
@@ -0,0 +1,60 @@
+/**
+ * @file Controller.h
+ * @brief モーター制御に用いる関数をまとめたラッパークラス
+ * @author takahashitom
+ */
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+#include "ev3api.h"
+#include "Motor.h"
+
+class Controller {
+ public:
+  static const int MOTOR_PWM_MAX = 100;
+  static const int MOTOR_PWM_MIN = -100;
+
+  Controller();
+
+  /**
+   * タイヤのモータにPWM値をセット
+   * @param pwm PWM値
+   */
+  void setRightMotorPwm(const int pwm);
+  void setLeftMotorPwm(const int pwm);
+
+  /**
+   * タイヤのモータを停止する
+   */
+  void stopMotor();
+
+  /**
+   * タイヤのモータのPWM値をリセットする
+   */
+  void resetMotorPwm();
+
+  /**
+   * アームのモータにPWM値をセット
+   * @param pwm PWM値
+   */
+  void setArmMotorPwm(const int pwm);
+
+  /**
+   * アームのモータを停止する
+   */
+  void stopArmMotor();
+
+ private:
+  ev3api::Motor rightWheel;
+  ev3api::Motor leftWheel;
+  ev3api::Motor armMotor;
+
+  /**
+   * モータに設定するPWM値の制限
+   * @param value 入力されたPWM値
+   * @return 制限されたPWM値
+   */
+  int limitPwmValue(const int value);
+};
+
+#endif
