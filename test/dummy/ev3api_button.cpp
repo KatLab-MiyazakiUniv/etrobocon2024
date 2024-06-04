d6680b702460ba0fb1ed4b67170adc0d1fc5cd03
KeiyaNimura <asxx1375@gmail.com>
2024-06-04 11:50:48 +0900

    delete: 冗長コードの削除

--- test/dummy/ev3api_button.cpp
+++ test/dummy/ev3api_button.cpp
@@ -6,35 +6,18 @@
 
 #include "ev3api_button.h"
 #include "../DummyRobot.h"
-#include <iostream>
 
 bool ev3_button_is_pressed(button_t button)
 {
   DummyRobot::ButtonState currentButtonState = DummyRobot::getButtonState();
 
-  //   std::cout << "Current ButtonState: ";
-  //   switch (currentButtonState) {
-  //       case DummyRobot::ButtonState::left:
-  //           std::cout << "Left" << std::endl;
-  //           break;
-  //       case DummyRobot::ButtonState::enter:
-  //           std::cout << "Enter" << std::endl;
-  //           break;
-  //       case DummyRobot::ButtonState::right:
-  //           std::cout << "Right" << std::endl;
-  //           break;
-  //       case DummyRobot::ButtonState::none:
-  //           std::cout << "None" << std::endl;
-  //           break;
-  //   }
-
-  // 左ボタンが押された時（num=0）
+  // 左ボタンが押された時
   if(button == LEFT_BUTTON && currentButtonState == DummyRobot::ButtonState::left) return true;
 
-  // 右ボタンが押された時（num=1）
+  // 右ボタンが押された時
   if(button == RIGHT_BUTTON && currentButtonState == DummyRobot::ButtonState::right) return true;
 
-  // 中央ボタンが押された時（num=2）
+  // 中央ボタンが押された時
   if(button == ENTER_BUTTON && currentButtonState == DummyRobot::ButtonState::enter) return true;
 
   if(button != LEFT_BUTTON && button != RIGHT_BUTTON && button != ENTER_BUTTON) {
