f829ac3257aa78d2dc0614c48fcdad51442343b7
takahashi <tkhs.tmhr39@gmail.com>
2024-06-17 15:22:15 +0900

    fix: 使用するメモリ領域の大きさの変更

--- module/Motion/PwmRotation.cpp
+++ module/Motion/PwmRotation.cpp
@@ -15,18 +15,18 @@ PwmRotation::PwmRotation(int _targetAngle, int _pwm, bool _isClockwise)
 
 bool PwmRotation::isMetPreCondition()
 {
-  char buf[LARGE_BUF_SIZE];  // log用にメッセージを一時保持する領域
+  char buf[SMALL_BUF_SIZE];  // log用にメッセージを一時保持する領域
 
   // pwm値が0以下の場合はwarningを出して終了する
   if(pwm <= 0) {
-    snprintf(buf, LARGE_BUF_SIZE, "The pwm value passed to PwmRotation is %d", pwm);
+    snprintf(buf, SMALL_BUF_SIZE, "The pwm value passed to PwmRotation is %d", pwm);
     logger.logWarning(buf);
     return false;
   }
 
   // targetAngleが0以下の場合はwarningを出して終了する
   if(targetAngle <= 0 || targetAngle >= 360) {
-    snprintf(buf, LARGE_BUF_SIZE, "The angle value passed to PwmRotation is %d", targetAngle);
+    snprintf(buf, SMALL_BUF_SIZE, "The angle value passed to PwmRotation is %d", targetAngle);
     logger.logWarning(buf);
     return false;
   }
@@ -53,10 +53,10 @@ bool PwmRotation::isMetContCondition(double targetLeftDistance, double targetRig
 
 void PwmRotation::logRunning()
 {
-  char buf[LARGE_BUF_SIZE];  // log用にメッセージを一時保持する領域
+  char buf[SMALL_BUF_SIZE];  // log用にメッセージを一時保持する領域
   const char* str = isClockwise ? "true" : "false";
 
-  snprintf(buf, LARGE_BUF_SIZE, "Run PwmRotation (angle: %d, pwm: %d, isClockwise: %s)",
+  snprintf(buf, SMALL_BUF_SIZE, "Run PwmRotation (angle: %d, pwm: %d, isClockwise: %s)",
            targetAngle, pwm, str);
   logger.log(buf);
 }
