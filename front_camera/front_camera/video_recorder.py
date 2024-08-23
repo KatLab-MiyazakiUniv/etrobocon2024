b41ab0508da05e309b168c5463fbf09c274b5ed0
CHIHAYATAKU <takuchi.1071880@gmail.com>
2024-08-23 08:57:50 +0100

    fix: 実機での動画取得のための修正

--- front_camera/front_camera/video_recorder.py
+++ front_camera/front_camera/video_recorder.py
@@ -1,40 +1,60 @@
+"""
+フロントカメラから動画を取得する
+@author: YKhm20020 bizyutyu
+"""
+
 import time
-import picamera
+from picamera2 import Picamera2
+from picamera2.encoders import H264Encoder
 
 
 class VideoRecorder:
     """カメラモジュールで動画を撮影するクラス"""
 
-    def __init__(self, resolution=(1920, 1080), framerate=30) -> None:
+    def __init__(self, camera_id: int = 0, resolution=(1920, 1080), framerate=30) -> None:
         """コンストラクタ"""
-        self.camera = picamera.PiCamera()
-        self.camera.resolution = resolution
-        self.camera.framerate = framerate
+        self.__camera_id = camera_id 
+        self.__resolution = resolution
+        self.__framerate = framerate
+        self.__camera = None
+    
+    def start_camera(self) -> None:
+        """Picamera2のインスタンス生成"""
+        if self.__camera is None:
+            camera = Picamera2()
+            video_config = camera.create_video_configuration()
+            # camera = Picamera2(camera_id = self.__camera_id)
+            # video_config = camera.create_video_configuration(main={"size": self.__resolution, "framerate": self.__framerate})
+            camera.configure(video_config)
+            self.__camera = camera
 
-    def start_recording(self) -> None:
+
+    def start_recording(self, encoder, output) -> None:
         """動画の撮影を開始する"""
-        self.camera.start_recording()
+        self.__camera.start_recording(encoder, output)
         print("Recording started")
 
     def stop_recording(self) -> None:
         """動画の録画を停止する"""
-        self.camera.stop_recording()
+        self.__camera.stop_recording()
         print("Recording stopped")
 
     def close(self) -> None:
         """カメラを終了する"""
-        self.camera.close()
+        self.__camera.close()
         print("Camera module closed for recording video")
 
 
 if __name__ == "__main__":
     video_recorder = VideoRecorder()
+    video_recorder.start_camera()
 
     try:
         # 出力する動画ファイルの名前
+        encoder = H264Encoder(bitrate=10000000)
         output_file_name = "recorded_video.h264"
 
-        video_recorder.start_recording(output_file_name)
+        video_recorder.start_recording(encoder, output_file_name)
         time.sleep(10)  # テストのため10秒間録画する
         video_recorder.stop_recording()
 
