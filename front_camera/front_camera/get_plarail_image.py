043fe9bbcca44dc2a1dcd7667144a72048e31226
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-08-30 16:36:53 +0900

    update: 他ファイルとクラスの命名を統一

--- front_camera/front_camera/get_plarail_image.py
+++ front_camera/front_camera/get_plarail_image.py
@@ -1,22 +1,27 @@
 """
 プラレール・背景撮影動画から画像を取得する
-@author: bizyutyu
+@author: bizyutyu YKhm20020
 """
 
 import cv2
 import os
+import time
 from frame_timing_calculator import FrameTimingCalculator
 from video_recorder import VideoRecorder
+from picamera2.encoders import H264Encoder
 
-class getPlarailImage:
+
+class GetPlarailImage:
     def __init__(self, video_path):
+        """コンストラクタ"""
         self.video_path = video_path
         self.frame_timing_calculator = FrameTimingCalculator(video_path)
 
-    def extract_target_frame(self, output_path):
+    def extract_target_frame(self, output_path) -> None:
+        """映像から特定のフレームを画像として切り出す"""
         # 目標フレームを取得
         target_frame = self.frame_timing_calculator.get_target_timing()
-        
+
         if target_frame is None:
             print("目標フレームの取得に失敗しました。")
             return
@@ -40,11 +45,14 @@ class getPlarailImage:
         # キャプチャを解放
         cap.release()
 
+
 if __name__ == "__main__":
     video_recorder = VideoRecorder()
     video_recorder.start_camera()
 
     try:
+        os.makedirs("video_data", exist_ok=True)
+
         # 出力する動画ファイルの名前
         encoder = H264Encoder(bitrate=10000000)
         output_video_path = "video_data/recorded_video.h264"
@@ -57,5 +65,5 @@ if __name__ == "__main__":
         video_recorder.close()
 
     output_image_path = "Pla.jpeg"
-    extractor = getPlarailImage(output_video_path)
+    extractor = GetPlarailImage(output_video_path)
     extractor.extract_target_frame(output_image_path)
