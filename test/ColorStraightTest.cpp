6219b175f4e9b4b44550e206a8d1e1845644fcdd
CHIHAYATAKU <takuchi.1071880@gmail.com>
2024-09-01 15:31:47 +0100

    fix: テストの修正

--- test/ColorStraightTest.cpp
+++ test/ColorStraightTest.cpp
@@ -116,7 +116,7 @@ namespace etrobocon2024_test {
     double actual = Mileage::calculateMileage(rightCount, leftCount);
 
     EXPECT_GT(expected, actual);          // 実行後に少しでも進んでいる
-    EXPECT_ET(expected + error, actual);  // 直進後の走行距離が許容誤差以内である
+    EXPECT_LE(expected + error, actual);  // 直進後の走行距離が許容誤差以内である
   }
 
   TEST(ColorStraightTest, runZeroPWM)
