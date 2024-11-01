ebd802004e4e02486a333350f5cac2b116049ec6
takuma <takuchi.1071880@gmail.com>
2024-11-01 14:44:25 +0900

    update: 赤のマスクを低色相と高色相の二つを付ける

--- front_camera/front_camera/color_rectangle_detector.py
+++ front_camera/front_camera/color_rectangle_detector.py
@@ -7,7 +7,8 @@ class ColorRectangleDetector:
     # 色のHSV範囲を定数として定義
     COLOR_BOUNDS = {
         'YELLOW': ((20, 160, 160), (30, 255, 255)),
-        'RED': ((0, 160, 160), (10, 255, 255)),    # 赤色の低色相側
+        'RED': [((0, 160, 160), (10, 255, 255)),    # 赤色の低色相側
+               ((170, 160, 160), (180, 255, 255))]   # 赤色の高色相側
     }
 
     def __init__(self, color):
@@ -19,18 +20,24 @@ class ColorRectangleDetector:
         if color not in self.COLOR_BOUNDS:
             raise ValueError(f"{color}は定義されていません。定義されている色: {list(self.COLOR_BOUNDS.keys())}")
 
-        self.lower_bound, self.upper_bound = self.COLOR_BOUNDS[color]
+        self.bounds = self.COLOR_BOUNDS[color]
         self.color = color
 
     def detect_rectangle(self, frame):
-        """対象色の矩形を検出する。
-        Args:
-            frame (int): 動画の何フレーム目を検出の対象とするかを示す数値
-        """
+    """対象色の矩形を検出する。
+    Args:
+        frame (numpy.ndarray): 処理対象の画像データ
+    """
         # HSV色空間に変換
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         # 対象の色のマスクを作成
-        mask = cv2.inRange(hsv, self.lower_bound, self.upper_bound)
+        if self.color == 'RED':
+            # 赤色の場合、両方の範囲のマスクを作成して結合
+            mask_red_lower = cv2.inRange(hsv, self.bounds[0][0], self.bounds[0][1])
+            mask_red_upper = cv2.inRange(hsv, self.bounds[1][0], self.bounds[1][1])
+            mask = cv2.bitwise_or(mask_red_lower, mask_red_upper)
+        else:
+            mask = cv2.inRange(hsv, self.bounds[0], self.bounds[1])
         # 輪郭を検出
         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         # 最大の輪郭を見つける
